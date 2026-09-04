b0a757404330dd2d5e9e428fd322609eb6ff801e
SebastianGeroli <sebastiangeroli@gmail.com>
2026-09-04 21:36:32 +0000

    Added html node and test

---
+++ src/htmlnode.py
@@ -0,0 +1,22 @@
+
+class HTMLNode:
+    def __init__(self, tag:str = None, value:str = None, children:list["HTMLNode"] = None, props:dict[str,str] = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+
+    def props_to_html(self):
+        props = ""
+        for item in self.props.items():
+            key, value = item
+            props +=f' {key}="{value}"'
+        return props
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag},{self.value},{self.children},{self.props})"
+
+    
---
+++ src/test_htmlnode.py
@@ -0,0 +1,20 @@
+import unittest
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+
+    def test_raise_not_NotImplementedError(self):
+       with self.assertRaises(NotImplementedError):
+           node = HTMLNode()
+           node.to_html()
+
+    def test_props(self):
+        node = HTMLNode(props={"target":"some_target"})
+        self.assertEqual(node.props_to_html(), ' target="some_target"')
+
+    def test_tag(self):
+            node = HTMLNode(tag="<a>")
+            self.assertEqual(node.tag,"<a>")
+            
+    
