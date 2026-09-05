04abd13c7e8ee2c2b45fae3ddf6906766ad6f76d
SebastianGeroli <sebastiangeroli@gmail.com>
2026-09-05 15:13:10 +0000

    add text to text nodes

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, split_nodes_delimiter, split_nodes_image, split_nodes_link
+from textnode import TextNode, TextType, split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -145,6 +145,21 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_full_cycle(self):
+        nodes = text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        expected = [
+                    TextNode("This is ", TextType.TEXT),
+                    TextNode("text", TextType.BOLD),
+                    TextNode(" with an ", TextType.TEXT),
+                    TextNode("italic", TextType.ITALIC),
+                    TextNode(" word and a ", TextType.TEXT),
+                    TextNode("code block", TextType.CODE),
+                    TextNode(" and an ", TextType.TEXT),
+                    TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                    TextNode(" and a ", TextType.TEXT),
+                    TextNode("link", TextType.LINK, "https://boot.dev"),
+                ]
+        self.assertEqual(nodes, expected)
 
 if __name__ == "__main__":
     unittest.main()
